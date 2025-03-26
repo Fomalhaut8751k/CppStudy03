2badd0e930a9131316e094706c487fe8992c35ef
WangZhongDe <1308041515@qq.com>
2025-03-26 23:54:39 +0800

    fifth commit

--- 10 BST树常见面试题/main.cpp
+++ 10 BST树常见面试题/main.cpp
@@ -253,6 +253,73 @@ public:
 		_findRange(_root, vec, left, right);
 	}
 
+	// 是否是BST树的判断
+	bool isBinarySearchTree()
+	{
+		// 依然是中序遍历，在中序遍历中，如果当前的值小于前面的值，那就不是BST树
+		Node* node = nullptr;
+		return _isBinarySearchTree(_root, node);
+	}
+
+	// 是否是子树的判断
+	bool isChildTree(BinarySearchTree<T,Compare>& bst)
+	{
+		// 先找bst的根结点
+		Node* it = _root;
+		while (it != nullptr)
+		{
+			if (comp(it->_nodeVal, bst._root->_nodeVal))  // 大于
+			{
+				it = it->_rightChild;
+			}
+			else if (comp(bst._root->_nodeVal, it->_nodeVal)) // 小于
+			{
+				it = it->_leftChild;
+			}
+			else  // 找到了根结点
+			{
+				// 让当前树跟着bst遍历
+				return _isChildTree(it, bst._root);
+			}
+		}
+		return false;  // 连根结点都没找到，肯定不是
+	}
+
+	// 最近公共祖先
+	int getLowestCommonAncestor(int L, int R)
+	{
+		// 先判断是否在树上
+		if (find(L) && find(R))
+		{
+			return _getLowestCommonAncestor(_root, L, R);
+		}
+		else
+		{
+			//throw "L和R没有公共祖先";
+			cout << "L和R没有公共祖先" << endl;
+			return -1;
+		}
+	}
+
+	// 镜像翻转
+	void mirrorInverse()
+	{
+		// 前序遍历
+		_mirrorInverse(_root);
+	}
+
+	// 镜像对称判断
+	bool mirrorSymmetry()
+	{
+		return _mirrorSymmetry(_root, _root);
+	}
+
+	// 由前序遍历和中序遍历复原二叉树
+	void restoreByPreorderandInOrder(int pre[], int i, int j, int in[], int m, int n)
+	{
+		_root = _restoreByPreorderandInOrder(pre, i, j, in, m, n);
+
+	}
 private:
 	struct Node
 	{
@@ -459,9 +526,300 @@ private:
 		}
 	}
 
+	bool _isBinarySearchTree(Node* it, Node* &pre)
+	{
+		// 原理还是中序遍历，要求严格递增(因为没有重复的)
+		// 判断到不是递增的就返回false
+		if (it == nullptr)  
+		{
+			return true;
+		}
+		if (!_isBinarySearchTree(it->_leftChild, pre))
+		{
+			return false;
+		}
+		if (pre != nullptr)  // pre判断是否为空，是避免第一个结点时没有pre
+		{
+			if (comp(it->_nodeVal, pre->_nodeVal))
+			{
+				return false;
+			}
+		}
+		pre = it; // 更新前一个结点
+		return _isBinarySearchTree(it->_rightChild, pre);
+	}
+
+	bool _isChildTree(Node* father, Node* child)
+	{
+		// 
+		if (father == nullptr && child == nullptr)
+		{
+			return true;
+		}
+		if (father == nullptr)
+		{
+			return false;
+		}
+		if (child == nullptr)
+		{
+			return true;
+		}
+		//else
+		//{
+		//	if (!_isChildTree(father->_leftChild, child->_leftChild))
+		//	{
+		//		return false;
+		//	}
+		//	if (father->_nodeVal != child->_nodeVal)
+		//	{
+		//		return false;
+		//	}
+		//	
+		//	if (!_isChildTree(father->_rightChild, child->_rightChild))
+		//	{
+		//		return false;
+		//	}
+		//}
+		if (father->_nodeVal != child->_nodeVal)
+		{
+			return false;
+		}
+		return _isChildTree(father->_leftChild, child->_leftChild)
+			&& _isChildTree(father->_rightChild, child->_rightChild);
+	}
+
+	int _getLowestCommonAncestor(Node* it, int L, int R)
+	{
+		if ((!comp(it->_nodeVal, L)) && (!comp(R, it->_nodeVal)))
+		{
+			return it->_nodeVal;
+		}
+		if (comp(it->_nodeVal, L))
+		{
+			return _getLowestCommonAncestor(it->_rightChild, L, R);
+		}
+		if (comp(R, it->_nodeVal))
+		{
+			return _getLowestCommonAncestor(it->_leftChild, L, R);
+		}
+		return it->_nodeVal;
+	}
+	
+	void _mirrorInverse(Node* it)
+	{
+		if (it == nullptr)
+		{
+			return;
+		}
+
+		Node* _tempChild = it->_leftChild;
+		it->_leftChild = it->_rightChild;
+		it->_rightChild = _tempChild;
+
+		_mirrorInverse(it->_leftChild);
+		_mirrorInverse(it->_rightChild);
+	}
+
+	bool _mirrorSymmetry(Node* it1, Node* it2)
+	{
+		if (it1 == nullptr && it2 == nullptr)
+		{
+			return true;
+		}
+		else if (it1 == nullptr || it2 == nullptr)
+		{
+			return false;
+		}
+		else
+		{
+			if (it1->_nodeVal != it2->_nodeVal)
+			{
+				return false;
+			}
+
+			return _mirrorSymmetry(it1->_leftChild, it2->_rightChild)
+				&& _mirrorSymmetry(it1->_rightChild, it2->_leftChild);
+		}
+	}
+
+	Node* _restoreByPreorderandInOrder(int pre[], int i, int j, int in[], int m, int n)
+	{
+		if (i > j || m > n)
+		{
+			return nullptr;
+		}
+		// 创建当前子树的根结点
+		Node* node = new Node(pre[i]);  // 拿前序的第一个数字创建子树根结点
+		for (int k = m; k <= n; ++k)
+		{
+			if (pre[i] == in[k])  // 在中序遍历中找子树根节点下标k
+			{
+				node->_leftChild = _restoreByPreorderandInOrder(
+					pre, i + 1, i + k - m, in, m, k - 1);
+				node->_rightChild = _restoreByPreorderandInOrder(
+					pre, i + k - m + 1, j, in, k + 1, n);
+				return node;
+			}
+		}
+		return node;
+	}
+
 	Node* _root;
 	Compare comp;
+
+	friend void test01();
+	friend void test02();
+	friend void test03();
+	friend void test04(int _preOrder[], int _inOrder[]);
 };
+
+// 创建一棵一般二叉树
+void test01()
+{
+	using Node = BinarySearchTree<int>::Node;
+	BinarySearchTree<int> bst;
+
+	bst._root = new Node(40);
+	Node* node_1 = new Node(20);
+	Node* node_2 = new Node(60);
+	Node* node_3 = new Node(30);
+	Node* node_4 = new Node(80);
+
+	bst._root->_leftChild = node_1;
+	bst._root->_rightChild = node_2;
+	node_2->_leftChild = node_3;
+	node_2->_rightChild = node_4;
+
+	//bst.foreachInorder();
+	// 中序遍历: 20 40 30 60 80
+	// 显然，不符合BST树中序遍历的性质
+
+	cout << "bst3是否是二分查找树: " << bst.isBinarySearchTree() << endl;
+}
+
+
+// 子树判断问题
+void test02() {
+	int arr[] = { 58, 24, 0, 5, 34, 41, 67, 62, 64, 69, 78 };
+	BinarySearchTree<int> bst1;
+	for (int i = 0; i < 11; ++i)
+	{
+		bst1.insert(arr[i]);
+	}
+
+	using Node = BinarySearchTree<int>::Node;
+	BinarySearchTree<int> bst;
+
+	bst._root = new Node(67);
+	Node* node_1 = new Node(62);
+	Node* node_2 = new Node(69);
+	Node* node_3 = new Node(30);
+
+	bst._root->_leftChild = node_1;
+	bst._root->_rightChild = node_2;
+
+	cout << "是否是子树: " << bst1.isChildTree(bst) << endl;
+
+	node_2->_leftChild = node_3;
+
+	cout << "是否是子树: " << bst1.isChildTree(bst) << endl;
+}
+
+// 镜像对称问题
+void test03()
+{
+	using Node = BinarySearchTree<int>::Node;
+	BinarySearchTree<int> bst;
+
+	bst._root = new Node(67);
+	Node* node_1 = new Node(31);
+	Node* node_2 = new Node(31);
+
+	bst._root->_leftChild = node_1;
+	bst._root->_rightChild = node_2;
+
+	cout << "\n是否镜像对称: " << bst.mirrorSymmetry() << endl;
+
+	Node* node_3 = new Node(30);
+	node_2->_leftChild = node_3;
+
+	cout << "插入元素后，是否镜像对称: " << bst.mirrorSymmetry() << endl;
+
+	Node* node_4 = new Node(30);
+	node_1->_rightChild = node_4;
+
+	cout << "对称补充相同元素后，是否镜像对称: " << bst.mirrorSymmetry() << endl;
+
+	Node* node_5 = new Node(12);
+	Node* node_6 = new Node(17);
+	node_1->_leftChild = node_5;
+	node_2->_rightChild = node_6;
+
+	cout << "对称插入不同元素后，是否镜像对称: " << bst.mirrorSymmetry() << endl;
+}
+
+// 由前序遍历和中序遍历复原二叉树
+/*
+	并不需要创建成全局变量，一样可以定义成类函数，然后初始化后调用
+*/
+BinarySearchTree<int>::Node* _restoreByPreorderandInOrder(
+	BinarySearchTree<int>::Node* it,
+	int _preOrder[], int _inOrder[], int& idxPre, int L, int R)
+{
+	if (L > R)
+	{
+		idxPre--;
+		return nullptr;
+	}
+	// 最小区间长度为1，则创建节点
+	if (L == R)
+		return new BinarySearchTree<int>::Node(_preOrder[idxPre]);
+	
+
+	// 寻找前序遍历中节点在中序遍历中对应的位置
+	int idxIn = L;
+	for (; idxIn < R; idxIn++)
+	{
+		if (_inOrder[idxIn] == _preOrder[idxPre])
+			break;
+	}
+
+	it = new BinarySearchTree<int>::Node(_preOrder[idxPre]);
+
+	// 递归
+	idxPre++;
+	it->_leftChild = _restoreByPreorderandInOrder(
+		it->_leftChild, _preOrder, _inOrder, idxPre, L, idxIn - 1);
+
+	idxPre++;
+	it->_rightChild = _restoreByPreorderandInOrder(
+		it->_rightChild, _preOrder, _inOrder, idxPre, idxIn + 1, R);
+
+	return it;
+}
+
+
+BinarySearchTree<int>::Node* restoreByPreorderandInOrder(
+	BinarySearchTree<int>::Node* it,
+	int _preOrder[], int _inOrder[], int idxPre, int L, int R)
+{
+	return _restoreByPreorderandInOrder(it, _preOrder, _inOrder, idxPre, L, R);
+}
+
+void test04(int _preOrder[], int _inOrder[])
+{
+	cout << "\n";
+	/*
+	int preorder[11] = { 58, 24, 0, 5, 34, 41, 67, 62, 64, 69, 78 };
+	int inorder[11] = { 0, 5, 24, 34, 41, 58, 62, 64, 67, 69, 78 };
+	*/
+	BinarySearchTree<int> _bst;
+	int length = 11;
+	_bst._root = restoreByPreorderandInOrder(
+		_bst._root, _preOrder, _inOrder, 0, 0, length - 1);
+	_bst.foreachPreorder();
+	_bst.foreachInorder();
+}
 
 int main()
 {
@@ -477,8 +835,8 @@ int main()
 		// 递归插入
 		bst2.insertRecursion(arr[i]);
 	}
-	bst1.foreachPostorder();
-	bst2.foreachPostorder();
+	bst1.foreachPreorder();
+	bst2.foreachInorder();
 	// 非递归查找
 	cout << "\nbst1是否存在67: " << bst1.find(67) << endl;
 	cout << "bst1是否存在5003: " << bst1.find(5003) << endl;
@@ -538,10 +896,52 @@ int main()
 			b. 若右子树不为空，则右子树上所有结点的值均大于它的根结点的值
 			c. 左右子树也分别满足二叉搜索树性质
 
+		特点：每一个结点都满足，值：左<结点<右，这个特点来判断只能判断局部
+		如下面的情况就不是一个bst树，但是符合性质
 			 60
 		20         80
 				30     110
 	*/
+	cout << "\n";
+	cout << "bst1是否是二分查找树: " << bst1.isBinarySearchTree() << endl;
+	cout << "bst2是否是二分查找树: " << bst2.isBinarySearchTree() << endl;
+	test01();
+
+// ###### 6. 二叉树求子树问题 ######################################################
+	cout << "\n";
+	test02();
+
+// ###### 7. 最近公共祖先 ##########################################################
+	/*                        58 
+				 24                       64		
+	        0         34            62          69
+			   5		  41	                    78
+	*/   
+	cout << "\n" << bst1.getLowestCommonAncestor(5, 41) << endl; // 应该是24
+	cout << bst2.getLowestCommonAncestor(62, 64) << endl; // 应该是64
+	bst1.getLowestCommonAncestor(12, 3);  // 没有公共祖先
+
+// ###### 8. 镜像翻转 ##############################################################
+	cout << "\n镜像翻转前的";
+	bst2.foreachInorder();
+	bst2.mirrorInverse();
+	cout << "镜像翻转后的";
+	bst2.foreachInorder();
+
+
+// ###### 9. 镜像对称判断 ##########################################################
+	test03();
+
+// ###### 10. 已知二叉树的前序遍历和中序遍历，重建二叉树 ###########################
+	int preorder[11] = { 58, 24, 0, 5, 34, 41, 67, 62, 64, 69, 78 };
+	int inorder[11] = { 0, 5, 24, 34, 41, 58, 62, 64, 67, 69, 78 };
+	cout << "\n我的方法: ";
+	test04(preorder, inorder);
+	cout << "\n视频的方法: " << endl;
+	BinarySearchTree<int> bst3;
+	bst3.restoreByPreorderandInOrder(preorder, 0, 10, inorder, 0, 10);
+	bst3.foreachPreorder();
+	bst3.foreachInorder();
 
 	return 0;
 }
